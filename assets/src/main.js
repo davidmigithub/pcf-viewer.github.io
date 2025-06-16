0ed165b2f761300f0ad2ec6bf1d89b3aef5d6800
FinSeifert <fin.seifert@rx-design.de>
2025-06-16 19:19:21 +0200

    .

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,36 +1,34 @@
-import { PcfParser } from "./parser.js";
-import { SceneBuilder } from "./scene-builder.js";
-import { UI } from './ui.js';
+import { PcfParser }        from "./parser.js";
+import { SceneBuilder }     from "./scene-builder.js";
 
-const ui = new UI();
 const builder = new SceneBuilder();
-ui.setBuilder(builder);
-const filesData = [];
 
 window.addEventListener('pcf-url', async (e) => {
-    ui.showSpinner();
-    try {
-        let url, fileName;
-        if (typeof e.detail === 'object') {
-            url = e.detail.url;
-            fileName = e.detail.name;
-        } else {
-            url = e.detail;
-            fileName = `Model ${filesData.length + 1}`;
-        }
-
-        if (!url) return;
-
-        const buffer = await fetch(url).then(res => res.arrayBuffer());
-        const text = new TextDecoder('utf-8').decode(buffer);
-        const parsed = new PcfParser(text).parse();
-
-        filesData.push({ parsed, fileName });
-        builder.addFile(parsed, fileName);
-        ui.buildSideMenu(filesData);
-    } catch (err) {
-        console.error('Error loading PCF:', err);
-    } finally {
-        ui.hideSpinner();
+  builder.ui.showSpinner();
+  try {
+    let url, fileName;
+    if (typeof e.detail === 'object') {
+      url      = e.detail.url;
+      fileName = e.detail.name;
+    } else {
+      url      = e.detail;
+      fileName = `Model ${builder.filesData.length + 1}`;
     }
+    if (!url) return;
+
+    const buffer = await fetch(url).then(r => r.arrayBuffer());
+    const text   = new TextDecoder('utf-8').decode(buffer);
+    const parsed = new PcfParser(text).parse();
+
+    builder.addFile(parsed, fileName);
+
+    const fileInput = document.querySelector('input[type=file]');
+    if (fileInput) fileInput.value = '';
+  }
+  catch (err) {
+    console.error('Error loading PCF:', err);
+  }
+  finally {
+    builder.ui.hideSpinner();
+  }
 });
